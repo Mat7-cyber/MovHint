20c6f49f6a73268c986ae89b803ad08f8c98e473
axel peron <axelperon4@gmail.com>
2024-11-27 10:12:52 +0100

    profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+alex = User.create(email: "alex@mail.com", password: "password", address: "2 avenue des saules, Lille 59000")
