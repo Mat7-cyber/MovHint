f00ed3e69b5d436882c3d635084169658a50a71b
alexandremorel <alexandremorel93@gmail.com>
2024-11-27 15:07:33 +0100

    page préférence en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Movie.create!(
 poster_path: "https://xl.movieposterdb.com/08_06/2008/468569/xl_468569_fe24b125.jpg",
 vote_average: 9.0,
 genres: "Thriller",
-release_date: "2008"
+release_date: "13 january 2008"
 )
 
 Movie.create!(
@@ -24,5 +24,21 @@ Movie.create!(
   poster_path: "https://xl.movieposterdb.com/22_11/2003/167260/xl_the-lord-of-the-rings-the-return-of-the-king-movie-poster_a977da10.jpg",
 vote_average: 8.0,
 genres: "Fantasy",
+release_date: "1 january 2003"
+)
+
+Movie.create!(
+  original_title: "The Lord of the Rings: The Return of the King",
+  poster_path: "https://xl.movieposterdb.com/20_06/2018/1677720/xl_1677720_376c9e0b.jpg",
+vote_average: 8.0,
+genres: "Fantasy",
+release_date: "2003"
+)
+
+Movie.create!(
+  original_title: "Harry potter",
+  poster_path: "https://xl.movieposterdb.com/21_03/1993/108052/xl_108052_bd91d0b6.jpg",
+vote_average: 5.0,
+genres: "Horror",
 release_date: "2003"
 )
