2ccb95e30857805b8a51fe14b5deb88b5d6aa25d
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <edouard.lassus@hotmail.fr>
2024-11-29 16:34:42 +0100

    Set-API-60%

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  before_action :set_tmdb, only: [:index]
 
   def index
     if "j'ai un param sarch"
@@ -17,4 +18,30 @@ class MoviesController < ApplicationController
       }
     @liked = false
   end
+
+  def search
+    query = params[:query]
+    if query.present?
+      url = "https://api.themoviedb.org/3/search/movie?query=#{query}&include_adult=false&language=en-US&page=1"
+      url = URI(url)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(url)
+      request["accept"] = 'application/json'
+      request["Authorization"] = "Bearer #{@token_key}"
+
+      response = http.request(request)
+      @results = JSON.parse(response.read_body)
+      @genres = Movie::MOVIE_GENRES
+    else
+      @results = []
+    end
+  end
+
+  private
+
+  def set_tmdb
+    @token_key = ENV["TMDB_TOKEN"]
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+require "uri"
+require "net/http"
+
+url = URI("https://api.themoviedb.org/3")
+Token_key = ENV["TMDB_TOKEN"]
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
