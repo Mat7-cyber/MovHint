064a48c92b6cfd3e9033f171f392db27a34282d4
axel peron <axelperon4@gmail.com>
2024-11-29 14:57:32 +0100

    cloudinary end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,15 @@ class UsersController < ApplicationController
 
 
   def update
+    current_user.photo.purge
+    current_user.update(photo: params[:user][:photo])
+    redirect_to dashboard_path
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:photo)
+  end
 
 end
