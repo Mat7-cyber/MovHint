95945f90d769443219bfa8090e24de557174ae7f
Mathieu Boulnois <mathieu@LAPTOP-0Q5QOS04.localdomain>
2024-12-05 09:45:17 +0100

    fix user dep

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   has_many :preferences
   has_many :suggestions
-  has_many :prefered_movies, through: :preferences, source: :movie
-  has_many :suggested_movies, through: :suggestions, source: :movie
+  # has_many :prefered_movies, through: :preferences, source: :movie
+  # has_many :suggested_movies, through: :suggestions, source: :movie
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
